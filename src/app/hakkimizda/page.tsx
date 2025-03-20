596f0a5e6a7efd96e212f0a2ffb06411512bbe2c
Mert48Ta <113031249+Mert48Ta@users.noreply.github.com>
2025-03-21 01:07:06 +0300

    my-first-upload

--- src/app/hakkimizda/page.tsx
+++ src/app/hakkimizda/page.tsx
@@ -3,9 +3,9 @@
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import WhatsAppButton from '@/components/WhatsAppButton'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import WhatsAppButton from '../../components/WhatsAppButton'
 
 export default function About() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from '@/components/Hero'
-import Navbar from '@/components/Navbar'
-import Services from '@/components/Services'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
-import WhatsAppButton from '@/components/WhatsAppButton'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+import Services from '../components/Services'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import WhatsAppButton from '../components/WhatsAppButton'
 import Image from 'next/image'
 
 export default function Home() {
